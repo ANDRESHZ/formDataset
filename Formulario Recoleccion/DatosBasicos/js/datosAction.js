1909d37bf4469042e06e910187a853d165eb2496
BENJAMIN HUERFANO <andreshuerfano11.3@gmail.com>
2022-01-30 07:45:26 -0300

    include form data an fill whit text
    include text fill whit a form data validatios of information, use of imgae an base 64 pdf file, allow to create a inform concern

--- Formulario Recoleccion/DatosBasicos/js/datosAction.js
+++ Formulario Recoleccion/DatosBasicos/js/datosAction.js
@@ -27,26 +27,25 @@ function validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
+var datafrom = new FormData();
 
-
-function logSubmit(event) {
-
-
-    var data = new FormData();
-    data.append("name", document.getElementById("element_1").value);
-    data.append("email", document.getElementById("element_3").value);
-    data.append("sex", document.getElementById("element_5").value);
-    data.append("phone", document.getElementById("element_6").value);
-    if (validateEmail(data.get('email'))) {
-        var valR = /\+/;
-        if (valR.test(data.get('phone')) || data.get('phone') == "" || data.get('phone') == null) {
-            data.append("date", `${document.getElementById("element_4_1").value}/${document.getElementById("element_4_2").value}/${document.getElementById("element_4_3").value}`);
-            var edad=diff_years(formatDate(thisMonth),data.get('date'));
+function logSubmit(event) {    
+    datafrom = new FormData();
+    datafrom.append("name", document.getElementById("element_1").value);
+    datafrom.append("email", document.getElementById("element_3").value);
+    datafrom.append("sex", document.getElementById("element_5").value);
+    datafrom.append("phone", document.getElementById("element_6").value);
+    if (validateEmail(datafrom.get('email'))) {
+        const valR = /\+/;
+        if (valR.test(datafrom.get('phone')) || datafrom.get('phone') == "" || datafrom.get('phone') == null) {
+            datafrom.append("date", `${document.getElementById("element_4_1").value}/${document.getElementById("element_4_2").value}/${document.getElementById("element_4_3").value}`);
+            var edad=diff_years(formatDate(thisMonth),datafrom.get('date'));
             if(edad>=18){
                 var ifConnected = window.navigator.onLine;
                 if (ifConnected) {
                     alert('Connection available');
-                    loga.textContent = `datos: ${data.get('name')}|${data.get('sex')}|${data.get('date')} (${edad})|${data.get('email')}|${data.get('phone')}`;
+                    var aaa=modifyPdf();
+                    loga.textContent = `datos: ${datafrom.get('name')}|${datafrom.get('sex')}|${datafrom.get('date')} (${edad})|${datafrom.get('email')}|${datafrom.get('phone')}`;
                 } else {
                     alert('Conexión a internet restringida o sin conexion');
                     loga.textContent = `Sin conexión a internet intente mas tarde: ${event.timeStamp}`;
@@ -59,12 +58,12 @@ function logSubmit(event) {
 
         } else {
             alert('Su número télefonico debe incluir el indicativo del pais EJ: +57321532000');
-            console.log(`su número debe incluir el indicativo del pais el actual: ${data.get('phone')}`)
+            console.log(`su número debe incluir el indicativo del pais el actual: ${datafrom.get('phone')}`)
         }
 
     } else {
         alert('Ajusta tu email: EjemploX@algo.net');
-        console.log(`Ajusta tu email: EjemploX@algo.net el actual ${data.get('email')}`)
+        console.log(`Ajusta tu email: EjemploX@algo.net el actual ${datafrom.get('email')}`)
     }
     event.preventDefault();
 }
@@ -76,4 +75,3 @@ var loga = document.getElementById('logaaa');
 forma.addEventListener('submit', logSubmit);
 
 
-var aaa=modifyPdf();
